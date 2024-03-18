1c78530357918ad41ae78fb52b66300a27880d5f
Đức Bình <binhbobinhbo22@gmail.com>
2024-03-18 20:59:03 +0700

    done dashboard,  fix format and date update

--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -1,4 +1,5 @@
-
+import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from 'react';
 import { faker } from '@faker-js/faker';
 
 import Container from '@mui/material/Container';
@@ -22,22 +23,74 @@ import { API_BASE_URL } from '../../../../config';
 // ----------------------------------------------------------------------
 
 export default function AppView() {
+  const [quotations, setQuotations] = useState([]);
+
+  useEffect(() => {
+    fetchQuotations();
+  }, []);
+
+  const fetchQuotations = async () => {
+    try {
+      const token = localStorage.getItem('jwttoken');
+      const response = await fetch(`${API_BASE_URL}/api/Quotation`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setQuotations(data);
+      } else {
+        console.error('Failed to fetch quotations');
+      }
+    } catch (error) {
+      console.error('Error fetching quotations:', error);
+    }
+  };
+
+  // Count quotations with status "Fully Invoice"
+  const countFullyInvoicedQuotations = () => quotations.filter(quotation => quotation.status === 'Fully Invoice').length;
+
+    // Count quotations with status "Fully Invoice"
+    const countQuotationQuotations = () => quotations.filter(quotation => quotation.status === 'Quotation').length;
+
+  // Count quotations with status "Fully Invoice" for each month
+  const countFullyInvoicedQuotationsByMonth = () => {
+    const monthlyCounts = Array(12).fill(0);
+
+    quotations.forEach(quotation => {
+      const month = new Date(quotation.createdAt).getMonth();
+      if (quotation.status === 'Fully Invoice') {
+        monthlyCounts[month] += 1;
+      }
+    });
+
+    return monthlyCounts;
+  };
+
+  const fullyInvoicedQuotationsByMonth = countFullyInvoicedQuotationsByMonth();
+
+
+  // Count quotations with status "Fully Invoice" for each month
+  const countCancelledQuotationsByMonth = () => {
+    const monthlyCounts = Array(12).fill(0);
+
+    quotations.forEach(quotation => {
+      const month = new Date(quotation.createdAt).getMonth();
+      if (quotation.status === 'Cancelled') {
+        monthlyCounts[month] += 1;
+      }
+    });
+
+    return monthlyCounts;
+  };
+
+  const cancelledQuotationsByMonth = countCancelledQuotationsByMonth();
+
   return (
     <Container maxWidth="xl">
-      <Typography variant="h4" sx={{ mb: 5 }}>
-        Hi, Welcome back 👋
-      </Typography>
-
       <Grid container spacing={3}>
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="Weekly Sales"
-            total={714000}
-            color="success"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
-          />
-        </Grid>
-
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
             title="New Users"
@@ -50,16 +103,16 @@ export default function AppView() {
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
             title="Item Orders"
-            total={1723315}
+            total={countFullyInvoicedQuotations()}
             color="warning"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
           />
         </Grid>
 
-        <Grid xs={12} sm={6} md={3}>
+        <Grid xs={12} sm={6} md={6}>
           <AppWidgetSummary
-            title="Bug Reports"
-            total={234}
+            title="Quotation Needs To Be Confirmed"
+            total={countQuotationQuotations()}
             color="error"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
           />
@@ -71,7 +124,7 @@ export default function AppView() {
             chart={{
               labels: [
                 '01/01/2024',
-                '02/01/2024', 
+                '02/01/2024',
                 '03/01/2024',
                 '04/01/2024',
                 '05/01/2024',
@@ -87,13 +140,13 @@ export default function AppView() {
                   name: 'Rejected',
                   type: 'area',
                   fill: 'gradient',
-                  data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+                  data: cancelledQuotationsByMonth,
                 },
                 {
                   name: 'Ordered',
                   type: 'area',
                   fill: 'gradient',
-                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
+                  data: fullyInvoicedQuotationsByMonth,
                 },
 
               ],
@@ -113,114 +166,7 @@ export default function AppView() {
               ],
             }}
           />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={8}>
-          <AppConversionRates
-            title="Conversion Rates"
-            subheader="(+43%) than last year"
-            chart={{
-              series: [
-                { label: 'Italy', value: 400 },
-                { label: 'Japan', value: 430 },
-                { label: 'China', value: 448 },
-                { label: 'Canada', value: 470 },
-                { label: 'France', value: 540 },
-                { label: 'Germany', value: 580 },
-                { label: 'South Korea', value: 690 },
-                { label: 'Netherlands', value: 1100 },
-                { label: 'United States', value: 1200 },
-                { label: 'United Kingdom', value: 1380 },
-              ],
-            }}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={4}>
-          <AppCurrentSubject
-            title="Current Subject"
-            chart={{
-              categories: ['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math'],
-              series: [
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
-              ],
-            }}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={8}>
-          <AppNewsUpdate
-            title="News Update"
-            list={[...Array(5)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: faker.person.jobTitle(),
-              description: faker.commerce.productDescription(),
-              image: `/assets/images/covers/cover_${index + 1}.jpg`,
-              postedAt: faker.date.recent(),
-            }))}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={4}>
-          <AppOrderTimeline
-            title="Order Timeline"
-            list={[...Array(5)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: [
-                '1983, orders, $4220',
-                '12 Invoices have been paid',
-                'Order #37745 from September',
-                'New order placed #XF-2356',
-                'New order placed #XF-2346',
-              ][index],
-              type: `order${index + 1}`,
-              time: faker.date.past(),
-            }))}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={4}>
-          <AppTrafficBySite
-            title="Traffic by Site"
-            list={[
-              {
-                name: 'FaceBook',
-                value: 323234,
-                icon: <Iconify icon="eva:facebook-fill" color="#1877F2" width={32} />,
-              },
-              {
-                name: 'Google',
-                value: 341212,
-                icon: <Iconify icon="eva:google-fill" color="#DF3E30" width={32} />,
-              },
-              {
-                name: 'Linkedin',
-                value: 411213,
-                icon: <Iconify icon="eva:linkedin-fill" color="#006097" width={32} />,
-              },
-              {
-                name: 'Twitter',
-                value: 443232,
-                icon: <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={32} />,
-              },
-            ]}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={8}>
-          <AppTasks
-            title="Tasks"
-            list={[
-              { id: '1', name: 'Create FireStone Logo' },
-              { id: '2', name: 'Add SCSS and JS files if required' },
-              { id: '3', name: 'Stakeholder Meeting' },
-              { id: '4', name: 'Scoping & Estimations' },
-              { id: '5', name: 'Sprint Showcase' },
-            ]}
-          />
-        </Grid>
+        </Grid>      
       </Grid>
     </Container>
   );
